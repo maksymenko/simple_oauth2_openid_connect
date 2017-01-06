69c4e7eef524399e878c15fec3d0f6f96b506d15
sergii <sergey.maksymenko@gmail.com>
2017-01-06 00:15:29 -0800

    https server handler

--- src/main/java/com/semaks/AuthHandler.java
+++ src/main/java/com/semaks/AuthHandler.java
@@ -8,12 +8,12 @@ import com.sun.net.httpserver.HttpHandler;
 
 public class AuthHandler implements HttpHandler {
 
-	@Override
-	public void handle(HttpExchange exchange) throws IOException {
-		String response = "Hello auth";
-		exchange.sendResponseHeaders(200, response.length());
-        OutputStream os = exchange.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
-	}
+  @Override
+  public void handle(HttpExchange exchange) throws IOException {
+    String response = "Hello auth";
+    exchange.sendResponseHeaders(200, response.length());
+    OutputStream os = exchange.getResponseBody();
+    os.write(response.getBytes());
+    os.close();
+  }
 }
